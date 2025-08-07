341dc6e4920e101887ff2f04f52f03977f2b97ed
M A Anson <ansonantony.work@gmail.com>
2025-08-07 23:03:13 +0530

    Update settings.py

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-g!y!^mz!os6(-0^-%q9o+vshj5-ct38fd41fh^+h#zqk@zho@l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['warehouse-tracker-web-app.onrender.com']
 
 
 # Application definition
