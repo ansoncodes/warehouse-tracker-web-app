dbc408fa9a1cc19b0704018db0794e3db7ed3585
M A Anson <ansonantony.work@gmail.com>
2025-08-09 14:10:36 +0530

    Update urls.py

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -1,14 +1,7 @@
+from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from . import views
-
-router = DefaultRouter()
-router.register(r'products', views.ProductViewSet)
-router.register(r'transactions', views.StckMainViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('inventory-summary/', views.InventorySummaryView.as_view(), name='inventory-summary'),
-    path('transactions/history/<str:product_name>/', views.product_transaction_history, name='product-transaction-history'),
+    path('admin/', admin.site.urls),
+    path('api/', include('inventory.urls')),
 ]
-
