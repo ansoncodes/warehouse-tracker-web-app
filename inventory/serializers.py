a59bbb81b84b6c2940687b55bf572dea14f4b542
M A Anson <ansonantony.work@gmail.com>
2025-08-09 15:32:37 +0530

    Update serializers.py

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import ProdMast, StckMain, StckDetail
 
+
 class ProdMastSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProdMast
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'sku', 'description']
+
 
 class StckDetailSerializer(serializers.ModelSerializer):
     product_id = serializers.IntegerField(source='product.id', read_only=True)
@@ -14,9 +16,32 @@ class StckDetailSerializer(serializers.ModelSerializer):
         model = StckDetail
         fields = ['id', 'product_id', 'product_name', 'quantity']
 
+
 class StckMainSerializer(serializers.ModelSerializer):
-    details = StckDetailSerializer(many=True, read_only=True)  # relies on related_name='details'
+    details = StckDetailSerializer(many=True, read_only=True)
 
     class Meta:
         model = StckMain
         fields = ['id', 'transaction_type', 'timestamp', 'details']
+
+
+# For creating transactions
+class StckDetailCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StckDetail
+        fields = ['product', 'quantity']
+
+
+class StckMainCreateSerializer(serializers.ModelSerializer):
+    details = StckDetailCreateSerializer(many=True)
+
+    class Meta:
+        model = StckMain
+        fields = ['transaction_type', 'details']
+
+    def create(self, validated_data):
+        details_data = validated_data.pop('details', [])
+        tx = StckMain.objects.create(**validated_data)
+        for d in details_data:
+            StckDetail.objects.create(transaction=tx, **d)
+        return tx
