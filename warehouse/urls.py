1a5792432c9d089903f2a4ba9f0c45d8de83f9d5
M A Anson <ansonantony.work@gmail.com>
2025-08-09 13:37:33 +0530

    Update urls.py

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from inventory.views import ProductViewSet, TransactionViewSet, InventorySummaryView
-
 
 urlpatterns = [
-path('admin/', admin.site.urls),
-path('api/', include('inventory.urls')),
+    path('admin/', admin.site.urls),
+    path('api/', include('inventory.urls')),  
 ]
+
